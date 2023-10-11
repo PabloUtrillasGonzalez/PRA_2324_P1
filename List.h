c39d468c0204647995bcbe88657f606abffaf811
Pablo Utrillas Gonzalez <putrgon@upv.edu.es>
2023-10-11 18:16:31 +0200

    Añadida interfaz generica List V2

--- List.h
+++ List.h
@@ -1,25 +1,29 @@
 #ifndef LIST_H
 #define LIST_H
 
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+
 template <typename T>
 
 class List {
 	public:
 
-		void insert(int pos, T e){}
+		virtual void insert(int pos, T e) = 0;
 
-		void append(T e){}
+		virtual void append(T e) = 0;
 
-		void prepend(T e){}
+		virtual void prepend(T e) = 0;
 
-		T remove(int pos){}
+		virtual T remove(int pos) = 0;
 
-		T get(int pos){}
+		virtual T get(int pos) = 0;
 
-		int search(T e){}
+		virtual int search(T e) = 0;
 
-		bool empty(){}
+		virtual bool empty() = 0;
 
-		int size(){}
+		virtual int size() = 0;
 };
 #endif
