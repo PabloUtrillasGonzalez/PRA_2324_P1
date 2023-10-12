a4c15f78ea4e681db8b83b113cf4bf5c36048c7f
Pablo Utrillas Gonzalez <putrgon@upv.edu.es>
2023-10-12 22:34:35 +0200

    Añadida implementación de la clase Point2D; Makefile actualizado

---
+++ Point2D.cpp
@@ -0,0 +1,25 @@
+#include <ostream>
+#include <cmath>
+#include "point2D.h"
+
+using namespace std;
+
+Point2D::Point2D(double x=0.0,double y=0.0) : x(x), y(y) {}
+
+double Point2D::Point2D::distance(const Point2D &a, const Point2D &b){
+	double dist = sqrt((pow((a.x - b.x),2) + pow((a.y-b.y),2)));
+	return dist;
+}
+
+bool operator==(const Point2D &a, const Point2D &b){
+	return ((a.x == b.x) && (a.y == b.y));
+}
+
+bool operator!=(const Point2D &a, const Point2D &b){
+	return ((a.x != b.x) || (a.y != b.y));
+}
+
+std::ostream& operator<<(std::ostream &out, const Point2D &p){
+	out << p.x << " " << p.y << endl;
+	return out;
+}
